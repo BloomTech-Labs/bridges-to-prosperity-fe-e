d90bbec7091420bf239aae916c6e85dadefc4277
meloegel <meloegel@gmail.com>
2020-10-12 18:28:19 -0400

    removing dead code

--- src/components/pages/DataViz/Markers.js
+++ src/components/pages/DataViz/Markers.js
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
-import { Marker, Popup } from 'react-map-gl';
+import React from 'react';
+import { Marker } from 'react-map-gl';
 import BridgeImage from './BridgeImage';
 
 const Markers = React.memo(({ bridgeData, setViewport }) => {
-  const [showPopup, setShowPopup] = useState(false);
-  const [selected, setSelected] = useState(0);
-  console.log('render');
   return (
     <>
       {bridgeData &&
@@ -13,41 +10,10 @@ const Markers = React.memo(({ bridgeData, setViewport }) => {
           return marker.Lat & marker.Long ? (
             <div key={index}>
               <Marker latitude={marker.Lat} longitude={marker.Long}>
-                <BridgeImage
-                  setViewport={setViewport}
-                  marker={marker}
-                  setSelected={setSelected}
-                  setShowPopup={setShowPopup}
-                  index={index}
-                />
+                <BridgeImage setViewport={setViewport} marker={marker} />
               </Marker>
             </div>
           ) : null;
-        })}
-
-      {showPopup &&
-        bridgeData.map((marker, index) => {
-          if (index === selected.index) {
-            return (
-              <div key={index}>
-                <Popup
-                  key={index}
-                  latitude={marker.Lat}
-                  longitude={marker.Long}
-                  anchor="bottom-right"
-                >
-                  <div className="popup">
-                    {/* This is the information where stackholder found them most valuable*/}
-                    <p>Province: {marker.province}</p>
-                    <p>District: {marker.district}</p>
-                    <p>Status: {marker.stage}</p>
-                    {/* bridge site name is coming soon */}
-                  </div>
-                </Popup>
-              </div>
-            );
-          }
-          return null;
         })}
     </>
   );
