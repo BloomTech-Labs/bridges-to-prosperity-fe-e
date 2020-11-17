44d699349ca40d4c2b06fc8e20b3f3d54766a4e2
Joe Bay <jbay55278@gmail.com>
2020-11-16 15:22:36 -0900

    Axios call to get visualizations from the DS database and to render it on the bridge card

--- src/components/pages/DataViz/Graphs/RenderGraph.js
+++ src/components/pages/DataViz/Graphs/RenderGraph.js
@@ -1,41 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
+import axios from 'axios';
 
-const RenderGraph = () => {
-  const trace1 = {
-    y: [5500, 200, 3302],
-    x: ['Total Population', 'Total Served', 'Growth'],
-    type: 'bar',
-    name: 'Social Effect',
-  };
-  const trace2 = {
-    y: [500, 3300, 302],
-    x: ['Total Population', 'Total Served', 'Growth'],
-    type: 'bar',
-    name: 'Economic Effect (USD)',
-  };
-  const trace3 = {
-    y: [5200, 3320, 3602],
-    x: ['Total Population', 'Total Served', 'Growth'],
-    type: 'bar',
-    name: 'Economic Effect (RWF)',
-  };
-
-  const data = [trace1, trace2, trace3];
-  const layout = {
-    width: 490,
-    height: 320,
-    font: { size: 11 },
-    showlegend: true,
-    legend: {
-      x: 0.5,
-      xanchor: 'center',
-      y: 1.6,
-    },
-    plot_bgcolor: '#161345',
-    color: 'white',
-  };
-
+const RenderGraph = props => {
+  const [dummy, setDummy] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        `http://btp-labs28-ds-e.eba-p8n7yppy.us-east-1.elasticbeanstalk.com/viz_nate1/{impact_score}?project_code=${props.data.project_code}`
+      )
+      .then(res => {
+        setDummy(JSON.parse(res.data));
+      });
+  }, []);
+  const data = dummy.data;
+  let layout = dummy.layout;
   return (
     <Plot
       className="graph"
